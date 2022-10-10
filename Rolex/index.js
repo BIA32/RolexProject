b5a64ec8222a1eed3b8677944871713465d9b9e8
Ben <baziz10sa@gmail.com>
2022-10-10 12:40:00 -0700

    rolex

--- Rolex/index.js
+++ Rolex/index.js
@@ -4,12 +4,18 @@ const secondEl = document.querySelector(".second")
 
 function updateClock(){
     const currentDate = new Date();
-    setTimeout(updateClock, 1000)
+//    setTimeout(updateClock, 1000)
     const hour = currentDate.getHours()
     const minute = currentDate.getMinutes()
     const second = currentDate.getSeconds()
     const hourDeg = (hour / 12) * 360;
     hourEl.style.transform = `rotate(${hourDeg}deg)`
+    const minuteDeg = (minute / 60) * 360;
+    minuteEl.style.transform = `rotate(${minuteDeg}deg)`
+    const secondDeg = (second / 60) * 360;
+    secondEl.style.transform = `rotate(${secondDeg}deg)`
 }
 
-updateClock();
+//updateClock();
+ 
+setInterval (updateClock, 1000) 
